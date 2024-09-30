a76331ce4a653b17fd80f10879094526a34750ad
Murilo Schneider <muriloedson2@gmail.com>
2024-09-30 17:11:07 -0300

    Desafio Concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Nokia:");
+Smartphone nokia = new Nokia("123456", "Nokia Tijolão", "11111111", 4);
+nokia.Ligar();
+nokia.InstalarAplicativo("zapzap");
+nokia.InformacoesSmartphone();
+
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Iphone:");
+Smartphone iphone = new Iphone("123456", "13 pro", "2222222", 256);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("tiktak");
+iphone.InformacoesSmartphone();
